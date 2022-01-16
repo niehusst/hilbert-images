9e85f6a11717c2713fd0605a9b24f6df6dede757
niehusst <liamniehusstaab@gmail.com>
2022-01-16 13:36:46 -0700

    draft hilbert alg

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import numpy as np
 imPath = "./cat.jpg"
 
 def transpose(mat, tl, br, alongTLBR):
-# TODO
-    pass
+    num_rots = 1 if alongTLBR else 3
+    mat[tl[0]:br[0],tl[1]:br[1]] = np.rot90(np.fliplr(mat[tl[0]:br[0],tl[1]:br[1]]), num_rots)
 
 def hilbertKernel(mat, links, tl, br):
     """
@@ -25,33 +25,33 @@ def hilbertKernel(mat, links, tl, br):
 
     # divide + conquer 4 quadrants
     mid = ((tl[0]+br[0])//2, (tl[1]+br[1])//2) 
-    hilbertKernel(mat, links, tl, mid)                          # tl
-    hilbertKernel(mat, links, (tl[0], mid[1]), (mid[0], br[1])) # tr
-    hilbertKernel(mat, links, (mid[0], tl[1]), (br[0], mid[1])) # bl
-    hilbertKernel(mat, links, mid, br)                          # br
+    hilbertKernel(mat, links, tl, mid)                            # q2 (tl)
+    hilbertKernel(mat, links, (tl[0], mid[1]+1), (mid[0], br[1])) # q1 (tr)
+    hilbertKernel(mat, links, (mid[0]+1, tl[1]), (br[0], mid[1])) # q3 (bl)
+    hilbertKernel(mat, links, (mid[0]+1, mid[1]+1), br)           # q4 (br)
 
-    # transpose tl, tr quadrants
+    # transpose q1 + q2 quadrants
     transpose(mat, tl, mid, True)
-    transpose(mat, (tl[0], mid[1]), (mid[0], br[1]), False)
+    transpose(mat, (tl[0], mid[1]+1), (mid[0], br[1]), False)
 
-    # TODO link start + end of each joined quadrant
+    # link start + end of each joined quadrant for path tracing
+    links[mat[mid[0]+1,tl[1]]] = mat[mid[0],tl[1]]   # tl q3 -> bl q2
+    links[mat[mid[0],mid[1]]] = mat[mid[0],mid[1]+1] # br q2 -> bl q1
+    links[mat[mid[0],br[1]]] = mat[mid[0]+1,br[1]]   # br q1 -> tr q4
 
 # use hilbert curves to make path through image
 def imageToFlatArray(image):
     # enforce dimensions dividable into 4x4 squares
     image = np.array(image)
     (height, width, _) = image.shape
-    print(f"w:{width} h:{height}")
     dim = max(width, height)
     dim += dim % 4 # make sure dims are divis by 4
-    print(f"d:{dim}")
     if not (dim == width == height):
         image = np.pad(image, ((0, dim-width), (0, dim-height), (0,0)), mode='reflect')
 
     # clone image matrix to create path;
-    # start with order1 hilbert curves
-    # TODO: redo w/ incremental numbers
-    pathMat = [[([2,3] if row & 1 else [1,4])[col & 1] for col in range(dim)] for row in range(dim)]
+    # TODO start with order1 hilbert curves (all nums unique, order inc w/in each curve)
+    pathMat = [[(row*dim)+col for col in range(dim)] for row in range(dim)] #doesnt draw the curves yet :\
     links = {}
     # run for side effects on pathMat
     hilbertKernel(pathMat, links, (0,0), (dim,dim))
